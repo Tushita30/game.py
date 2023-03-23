a286909db49c71aa2a005e8b1e0ad4fbbe371ff9
vanshika945 <122337924+vanshika945@users.noreply.github.com>
2023-03-23 15:16:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,12 @@ player = False
 cpu_score = 0
 player_score = 0
 while True:
+    player = input("Rock, Paper or  Scissors?").capitalize()
+    ## Conditions of Rock,Paper and Scissors
+    if player == computer:
+        print("Tie!")
+    elif player == "Rock":
+        if computer == "Paper":
+            print("You lose!", computer, "covers", player)
+            cpu_score+=1
 
