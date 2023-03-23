54f84068b1e27b25f7ce2ca6c43597c5311ef026
udayveer923 <124018933+udayveer923@users.noreply.github.com>
2023-03-23 15:31:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,4 +13,26 @@ while True:
         if computer == "Paper":
             print("You lose!", computer, "covers", player)
             cpu_score+=1
+        else:
+            print("You win!", player, "smashes", computer)
+            player_score+=1
+    elif player == "Paper":
+        if computer == "Scissors":
+            print("You lose!", computer, "cut", player)
+            cpu_score+=1
+        else:
+            print("You win!", player, "covers", computer)
+            player_score+=1
+    elif player == "Scissors":
+        if computer == "Rock":
+            print("You lose...", computer, "smashes", player)
+            cpu_score+=1
+        else:
+            print("You win!", player, "cut", computer)
+            player_score+=1
+    elif player=='End':
+        print("Final Scores:")
+        print(f"CPU:{cpu_score}")
+        print(f"player:{player_score}")
+        break
 
